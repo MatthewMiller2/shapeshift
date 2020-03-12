e4c3aa1297ce25f831d67452aee38c162c6c6984
Matthew Miller <matthew.miller@appian.com>
2020-03-12 17:21:50 -0400

    Camel case in getters and setters

--- src/main/java/ShapeShift.java
+++ src/main/java/ShapeShift.java
@@ -1,6 +1,7 @@
 import org.stringtemplate.v4.ST;
 import org.stringtemplate.v4.STGroup;
 import org.stringtemplate.v4.STGroupFile;
+import org.stringtemplate.v4.StringRenderer;
 
 public class ShapeShift {
 
@@ -11,6 +12,7 @@ public class ShapeShift {
     person.addField(new Field("int", "age"));
 
     STGroup group = new STGroupFile("class.stg");
+    group.registerRenderer(String.class, new StringRenderer());
     ST st = group.getInstanceOf("entry");
     st.add("schema", person);
 
